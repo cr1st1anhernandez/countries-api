19cb75bf9e85838be48c09c14996b91dcfe33be8
Cristian Neftali Hernandez Ramirez <cristian0402218@gmail.com>
2024-02-14 10:59:16 -0600

    :bento: Change assets path and Add new assets

--- src/app/components/HomePage.tsx
+++ src/app/components/HomePage.tsx
@@ -1,7 +1,7 @@
 import { Button, Image, Link } from "@nextui-org/react";
 import { IoIosArrowForward } from "react-icons/io";
 
-export default function HomePage():JSX.Element {
+export default function HomePage(): JSX.Element {
   return (
     <main className="max-w-[1500px] mx-auto px-6 pt-8 xl:pt-32">
       <div className="flex flex-col xl:flex-row justify-between items-center">
@@ -14,7 +14,7 @@ export default function HomePage():JSX.Element {
             View Docs
           </Button>
         </article>
-        <Image src="/principalImage.svg" width={600} height={600} alt="PrincipalImage" />
+        <Image src="/assets/images/principalImage.svg" width={600} height={600} alt="PrincipalImage" />
       </div>
       <div className="flex flex-col xl:flex-row justify-end items-center">
         <p className="text-xl text-zinc-500 font-bold text-pretty">Inspired by <Link isExternal href="https://fakeapi.platzi.com/" className="text-teal-500 text-xl">FakeStoreApi</Link></p>
